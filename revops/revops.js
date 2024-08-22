2578acd113b46fa7bf5f58acb25016df78f01167
maceq687 <maciek@softman.pl>
2024-08-22 13:36:30 +0200

    Rebuilt code resposnsible for building HTML structure of services and recent blog posts on revops page

--- revops/revops.js
+++ revops/revops.js
@@ -6,45 +6,22 @@ async function populateServices() {
   const response = await fetch(request);
   const services = await response.json();
 
-  const servicesList = document.createElement("div");
-  servicesList.classList.add("collection-list", "w-dyn-items");
-
-  services.forEach((service) => {
-    const serviceItem = document.createElement("div");
-    serviceItem.classList.add("collection-item", "w-dyn-item");
-
-    const serviceItemLink = document.createElement("a");
-    serviceItemLink.href = service.Slug;
-    serviceItemLink.classList.add("link-block", "w-inline-block");
-
-    const serviceItemContainer = document.createElement("div");
-    serviceItemContainer.classList.add("div-block");
-
-    const serviceItemIcon = document.createElement("img");
-    serviceItemIcon.src = service.Icon;
-    serviceItemIcon.classList.add("image");
-
-    const serviceItemTitle = document.createElement("h3");
-    serviceItemTitle.innerHTML = service.Name;
-    serviceItemTitle.classList.add("post-preview-title");
-
-    const serviceItemSummary = document.createElement("p");
-    serviceItemSummary.innerHTML = service["Short description"];
-    serviceItemSummary.classList.add("post-summary");
-
-    serviceItemContainer.append(
-      ...[serviceItemIcon, serviceItemTitle, serviceItemSummary]
-    );
-
-    serviceItemLink.appendChild(serviceItemContainer);
-
-    serviceItem.appendChild(serviceItemLink);
-
-    servicesList.appendChild(serviceItem);
-  });
-
-  const container = document.getElementById("services-list");
-  container.appendChild(servicesList);
+  const servicesList = document.getElementById("services");
+  servicesList.innerHTML = services
+    .map(
+      (service) => `
+        <div class="collection-item w-dyn-item">
+          <a href="${service.Slug}" class="link-block w-inline-block">
+            <div class="div-block">
+              <img src="${service.Icon}" loading="lazy" alt="" class="image"/>
+              <h3 class="post-preview-title">${service.Name}</h3>
+              <p class="post-summary">${service["Short description"]}</p>
+            </div>
+          </a>
+        </div>
+      `
+    )
+    .join("");
 }
 
 async function populateBlogPosts() {
@@ -68,25 +45,27 @@ async function getLatestBlogPosts() {
   latestBlogPosts = latestBlogPosts.slice(0, 3);
 
   const blogPostsList = document.getElementById("recent-blog-posts");
-  const latestBlogPost = document.getElementById("post-preview");
 
-  latestBlogPosts.forEach((post) => {
-    const clone = latestBlogPost.content.cloneNode(true);
-
-    const blogPostLink = clone.getElementById("post-preview-link");
-    blogPostLink.href = post.Slug;
-
-    const blogPostThumbnail = clone.getElementById("post-preview-thumbnail");
-    blogPostThumbnail.src = post["Thumbnail image"];
-
-    const blogPostTitle = clone.getElementById("post-preview-title");
-    blogPostTitle.innerHTML = post.Name;
-
-    const blogPostSummary = clone.getElementById("post-preview-summary");
-    blogPostSummary.innerHTML = post["Post Summary"];
-
-    blogPostsList.appendChild(clone);
-  });
+  blogPostsList.innerHTML = latestBlogPosts
+    .map(
+      (post) => `
+        <div class="post-preview w-dyn-item">
+          <a href="${post.Slug}" class="post-link-block w-inline-block">
+            <div class="div-block-2">
+              <img
+                src="${post["Thumbnail image"]}"
+                loading="lazy"
+                alt=""
+                class="post-thumbnail"
+              />
+              <h6 class="post-preview-title">${post.Name}</h6>
+              <p class="post-summary">${post["Post Summary"]}</p>
+            </div>
+          </a>
+        </div>
+      `
+    )
+    .join("");
 }
 
 populateServices();
